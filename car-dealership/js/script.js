4ebbd772bc520e7e1192e95f7e792369ef3566d2
cisu <49948070+cisu@users.noreply.github.com>
2021-02-24 13:31:25 +0200

    display featured cars

--- car-dealership/js/script.js
+++ car-dealership/js/script.js
@@ -51,23 +51,16 @@ const CreateCars = (() => {
 
   // produce cars
   function produceCars() {
-    makeCar('chevron', 'american');
     makeCar('mercedes', 'german', 'img/car-german-1.jpeg', true);
-    makeCar('mercedes', 'german', 'img/car-german-2.jpeg', true);
-    makeCar('mercedes', 'german', 'img/car-german-3.jpeg', false, 'some model');
-    makeCar(
-      'mercedes',
-      'german',
-      'img/car-german-4.jpeg',
-      undefined,
-      'other model'
-    );
+    makeCar('bmw', 'german', 'img/car-german-2.jpeg', true);
+    makeCar('bmw', 'german', 'img/car-german-3.jpeg', false, 'some model');
+    makeCar('bmw', 'german', 'img/car-german-4.jpeg', undefined, 'other model');
     makeCar('mercedes', 'german', 'img/car-german-5.jpeg', false);
     makeCar('chevy', 'american', 'img/car-american-1.jpeg');
     makeCar('chevy', 'american', 'img/car-american-2.jpeg', false);
     makeCar('chevy', 'american', 'img/car-american-3.jpeg', false);
     makeCar('chevy', 'american', 'img/car-american-4.jpeg', false);
-    makeCar('chevy', 'american', 'img/car-american-5.jpeg', false);
+    makeCar('chevy', 'american', 'img/car-american-5.jpeg', true);
   }
 
   produceCars();
@@ -83,5 +76,39 @@ const CreateCars = (() => {
   };
 })();
 
-console.log(CreateCars.cars);
-console.log(CreateCars.specialCars);
+const DisplaySpecialCars = ((CreateCars) => {
+  const specialCars = CreateCars.specialCars;
+  console.log(specialCars);
+
+  const info = document.querySelector('.featured-info');
+
+  // document loader event
+  document.addEventListener('DOMContentLoaded', () => {
+    info.innerHTML = '';
+
+    let data = '';
+
+    specialCars.forEach((item) => {
+      data += `
+      <!-- single item -->
+      <div class="featured-item my-3 d-flex p-2 text-capitalize align-items-baseline flex-wrap">
+        <span data-img='${item.img}' class="featured-icon mr-2">
+          <i class="fas fa-car"></i>
+        </span>
+        <h5 class="font-weight-bold mx1">${item.make}</h5>
+        <h5 class="mx-1">${item.model}</h5>
+      </div>
+      <!-- end of single item -->
+      `;
+    });
+    info.innerHTML = data;
+  });
+
+  // change img
+  info.addEventListener('click', (e) => {
+    if (e.target.parentElement.classList.contains('featured-icon')) {
+      const img = e.target.parentElement.dataset.img;
+      document.querySelector('.featured-photo').src = img;
+    }
+  });
+})(CreateCars);
